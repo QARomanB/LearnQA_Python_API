1bef636258cc650d5c0eeafe504122eb7e42d413
QA-1 <Alfa-tester@outlook.com>
2024-05-02 13:10:03 +0300

    Updating the file with the ex 4 answer

--- PycharmProjects/LearnQA_TestPythonAPI/main.py
+++ PycharmProjects/LearnQA_TestPythonAPI/main.py
@@ -1,4 +1,16 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+
+def main():
+    url = "https://playground.learnqa.ru/api/get_text"
+
+    # Otpravka get zaprosa
+    response = requests.get(url)
+
+    # Vivod Tela otveta
+    print("Response body:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
