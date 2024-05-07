ad2c6fcca3858aca7b8ad8de4abf293a241b2eff
QARomanB <beleccir@gmail.com>
2024-05-07 09:16:44 +0300

    Update main.py

--- PycharmProjects/LearnQA_TestPythonAPI/main.py
+++ PycharmProjects/LearnQA_TestPythonAPI/main.py
@@ -1,4 +1,18 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+def main():
+    url = "https://playground.learnqa.ru/api/get_text"
+
+    # отправка гет запроса
+    response = requests.get(url)
+
+    # проверка статус кода
+    if response.status_code == 200:
+        # выводд ответа
+        print("Response body:")
+        print(response.text)
+    else:
+        print(f"Failed to get response. Status code: {response.status_code}")
+
+if __name__ == "__main__":
+    main()
