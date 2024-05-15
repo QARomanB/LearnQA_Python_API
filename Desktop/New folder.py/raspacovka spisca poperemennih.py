1489780d351a26ad372af2a7ee3bcccf82c728fc
QA-1 <Alfa-tester@outlook.com>
2024-05-15 10:07:48 +0300

    adding parsi json code

---
+++ Desktop/New folder.py/raspacovka spisca poperemennih.py
@@ -0,0 +1,4 @@
+z, x, *c, = [1, 2, 3, 5, 6]
+print(z)
+print(x)
+print(c)
---
+++ Desktop/New folder.py/types1.py
@@ -0,0 +1,5 @@
+x = float (input ('itroduceti ceva '))
+z = input ('introduceti un numar zecimal ')
+r = (x) + float(z)
+
+print ('result' + str(r))
