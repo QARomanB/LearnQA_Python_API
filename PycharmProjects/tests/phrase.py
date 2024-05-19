0072751376cacb8903158990ea22696f62f5d9bd
QARomanB <beleccir@gmail.com>
2024-05-19 11:59:14 +0300

    Update phrase.py

--- PycharmProjects/tests/phrase.py
+++ PycharmProjects/tests/phrase.py
@@ -1,15 +1,15 @@
 def test_input_length():
     # Ask user for input
-    phrase = input("Set a phrase")
+    phrase = input("Set a phrase with less then 15 characters: ")
 
     # Determine length of input
     input_length = len(phrase)
 
-    # Compare input length with determined length
+    # Validate that the input is less then 15 character long
     input_length = 15
-    assert input_length == len(phrase), "Length of input doesn't match the determined length"
+    assert input_length > len(phrase), "Length of input doesn't match the determined length"
 
-    print("Input length matches determined length")
+    print("Input phrase length is less then 15 and matches the determined length")
 
 if __name__ == "__main__":
     test_input_length()
