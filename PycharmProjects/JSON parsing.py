c6375fdfde8759b3e6b0b6e592d9286c33cf4297
QARomanB <beleccir@gmail.com>
2024-05-25 13:24:35 +0300

    Delete PycharmProjects/JSON parsing.py

--- PycharmProjects/JSON parsing.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_data = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-
-parsed_data = json.loads(json_data)
-
-second_message_text = parsed_data['messages'][1]['message']
-print(second_message_text)
