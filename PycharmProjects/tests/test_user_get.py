e1b6f57ea6ff73610baf3b6be817f6d227871422
QARomanB <beleccir@gmail.com>
2024-05-26 12:37:58 +0300

    Update test_user_get.py

--- PycharmProjects/tests/test_user_get.py
+++ PycharmProjects/tests/test_user_get.py
@@ -32,4 +32,28 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_keys(response2, expected_fields)
 
 
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        other_user_id = 1
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{other_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+
+        response2 = requests.get("https://playground.learnqa.ru/api/user/2")
+        Assertions.assert_json_has_key(response2, 'username')
+        Assertions.assert_json_has_no_key(response2, 'email')
+        Assertions.assert_json_has_no_key(response2, 'firstName')
+        Assertions.assert_json_has_no_key(response2, 'lastName')
+        
 
